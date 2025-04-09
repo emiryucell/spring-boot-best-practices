1bebeb3368b830de76fa3bbfcd6f146b6ff0b1db
ety1 <ety1@student.le.ac.uk>
2025-04-09 15:57:29 +0100

    Initial project structure created.

---
+++ src/main/java/com/emiryucel/courseportal/CoursePortalApplication.java
@@ -0,0 +1,13 @@
+package com.emiryucel.courseportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePortalApplication.class, args);
+    }
+
+}
